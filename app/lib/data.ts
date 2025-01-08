4b693dcac0b25f0b17397d987722ceb9ee3fd01b
Grzegorz Wasylów <g.wasylow@gmail.com>
2025-01-09 00:41:12 +0100

    Databse update and fetching

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -60,6 +60,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+    //make the queries run in parallel     
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
